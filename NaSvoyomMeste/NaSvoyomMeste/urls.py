3fc22f8efed169bff54019fd197ac6003e0af9e2
Nikita <pozidaevnikita85@gmail.com>
2024-03-19 21:03:37 +0300

    Добавил чуть верстки

--- NaSvoyomMeste/NaSvoyomMeste/urls.py
+++ NaSvoyomMeste/NaSvoyomMeste/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('posts.urls')),
+    path('group/<slug:slug>/', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
--- NaSvoyomMeste/posts/views.py
+++ NaSvoyomMeste/posts/views.py
@@ -6,4 +6,18 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
